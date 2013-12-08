8c4416a5957e0dbd2cbc3e14d1999016dc2a8559
Nicolas Drebenstedt <nd026@hdm-stuttgart.de>
2013-12-08 20:19:48 +0100

    Clean up code style

--- logstash/outputs/hbase.rb
+++ logstash/outputs/hbase.rb
@@ -1,45 +1,45 @@
 # encoding: utf-8
-require "logstash/outputs/base"
-require "logstash/namespace"
+require 'logstash/outputs/base'
+require 'logstash/namespace'
 
 class LogStash::Outputs::HBase < LogStash::Outputs::Base
 
-  config_name "hbase"
+  config_name 'hbase'
   milestone 2
 
   # The HBase server uri
-  config :uri, :validate => :string, :required => false, :default => "localhost"
-  
+  config :uri, :validate => :string, :required => false, :default => 'localhost'
+
   # The table name to use
   config :table_name, :validate => :string, :required => true
 
   # Optionally define columns
-  config :columns, :validate => :array, :require => false, :default => ["cf1"]
+  config :columns, :validate => :array, :require => false, :default => ['cf1']
 
   public
   def register
-    require "rubygems"
-    require "hbaserb"
+    require 'rubygems'
+    require 'hbaserb'
 
     client = HBaseRb::Client.new(@uri)
-    @logger.debug("Register: established hbase connection")
+    @logger.debug('Register: established hbase connection')
 
-    if not client.has_table?(@table_name)
-      @table = client.get_table(@table_name)
-    else
+    if client.has_table?(@table_name)
       args = [@table_name] + @columns
       @table = client.create_table(*args)
+    else
+      @table = client.get_table(@table_name)
     end
   end
 
   public
   def receive(event)
     document = event.to_hash
-    @table.mutate_row(document["_id"], {"cf1:pref" => document["message"]})
+    @table.mutate_row(document['_id'], {'cf1:pref' => document['message']})
   end
 
   def teardown
     @client.close
-    @logger.debug("Teardown: closed hbase connection")
+    @logger.debug('Teardown: closed hbase connection')
   end
 end
