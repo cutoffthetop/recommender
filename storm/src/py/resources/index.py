7c1a630057ce78f36c1d2406bb7109e72dee4ee3
Nicolas Drebenstedt <nd026@hdm-stuttgart.de>
2014-01-03 17:18:10 +0100

    Add dynamic mapping creation with nested type

--- storm/src/py/resources/index.py
+++ storm/src/py/resources/index.py
@@ -1,19 +1,44 @@
 # -*- coding: utf-8 -*-
 
 from elasticsearch import Elasticsearch
+from elasticsearch.client import IndicesClient
 from elasticsearch.exceptions import NotFoundError
+from elasticsearch.exceptions import TransportError
 from storm import Bolt, log
 
 
 class ESIndexBolt(Bolt):
     def initialize(self, conf, context):
-        log('bolt initializing')
+        log('Bolt id-%s is initializing.' % id(self))
         # TODO: Make connection params configurable.
-        self.es = Elasticsearch(hosts={'host': 'localhost', 'port': 9200})
+        self.es = Elasticsearch(hosts=[{'host': 'localhost', 'port': 9200}])
+        ic = IndicesClient(self.es)
+
+        try:
+            ic.get_mapping(
+                index='observations',
+                doc_type='user'
+                )
+        except NotFoundError:
+            doc_type = {
+                'user': {
+                    'properties': {
+                        'events': {
+                            'type': 'nested'
+                            }
+                        }
+                    }
+                }
+            ic.put_mapping(
+                index='observations',
+                ignore_conflicts=True,
+                doc_type='user',
+                body=doc_type
+                )
 
     def process(self, tup):
         event = dict(
-            timestamp=int(tup.values[0]) * 1000,
+            timestamp=tup.values[0],
             path=tup.values[1]
             )
 
@@ -24,11 +49,14 @@ class ESIndexBolt(Bolt):
             )
 
         try:
-            events = self.es.get(**params)['_source']['history']
-            params['body'] = {'history': events + [event]}
+            events = self.es.get(**params)['_source']['events']
+            params['body'] = {'events': events + [event]}
         except NotFoundError:
-            params['body'] = {'history': [event]}
+            params['body'] = {'events': [event]}
             params['op_type'] = 'create'
+        except TransportError, e:
+            # TODO: What is going wrong here?
+            log('[TransportError] %s' % e)
 
         self.es.index(**params)
 
